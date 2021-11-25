7b8cb4f5f8f61dc01d7bbdb8e48c59edc66a317a
BOTZ4YOU <91576548+BOTZ4YOU@users.noreply.github.com>
2021-11-25 08:24:52 +0700

    Add files via upload

---
+++ shop/jadibot.js
@@ -0,0 +1,41 @@
+const jadibut = () =>{
+return`╭──「 📖  *PRICE JADIBOT*」
+│
+├ • *Test Jadibot [15-30m]*
+│
+│ ⤷ *Free* 💌
+│
+├ • *Permanen*
+│
+│ ⤷ *Harga :  25k [E-Wallet]*
+│ ⤷ *Harga :  30k [Pulsa]*
+│
+├─ 「 💰  *PAYMENT* 」
+│
+│ ⤷ *Gopay [Diutamakan]*
+│ ⤷ *Dana*
+│ ⤷ *Ovo*
+│ ⤷ *Qris All Pay [+Pajak/Biaya Admin]*
+│ ⤷ *Pulsa IM3/Indosat [+5K]*
+│ ⤷ *Pulsa Smartfren [+5K]*
+│
+├─ 「 👨‍💻  *RUN ON* 」
+│
+│ ⤷ *Heroku [Diutamakan]*
+│ ⤷ *Termux*
+│ ⤷ *Railway* 
+│
+├─ 「 📝  *NOTES* 」
+│
+│ ⤷ _Tidak Perlu Termux_
+│ ⤷ _Bot Jdi Fast Respon_
+│ ⤷ _Minat?? wa.me/62857726746954
+│ ⤷ _Testi Cek Ktalog Nomer Di atas👆🏻_
+│ ⤷ _Ragu?? Beli Yang Murah aja dlu_
+│ ⤷ _Msih Ragu?? Testi Sgtu Jibunnya_
+│ _Msih Ragu🥵_
+│
+╰──「 *Thank's* 」
+`
+}
+exports.jadibut = jadibut
